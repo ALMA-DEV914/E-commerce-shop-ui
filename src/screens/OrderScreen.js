2b103a61d753596097fcd71bf3c3f910e9a3f295
Alma Braun <aungonalna58@gmail.com>
2023-05-25 23:39:16 -0700

    Update OrderScreen.js

--- src/screens/OrderScreen.js
+++ src/screens/OrderScreen.js
@@ -34,7 +34,7 @@ const OrderScreen = ({ match }) => {
 
   useEffect(() => {
     const addPayPalScript = async () => {
-      const { data: clientId } = await axios.get(`${URL}/api/config/paypal`);
+      const { data: clientId } = await axios.get(`https://e-commerce-shop-a7lc.onrender.com/api/config/paypal`);
       const script = document.createElement("script");
       script.type = "text/javascript";
       script.src = `https://www.paypal.com/sdk/js?client-id=${clientId}`;
