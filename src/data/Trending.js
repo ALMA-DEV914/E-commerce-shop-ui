cd34d9e6326b5c9cd3182b7a3cb52e97c60596dd
Alma Braun <aungonalna58@gmail.com>
2023-06-09 10:36:22 -0700

    Update Trending.js

--- src/data/Trending.js
+++ src/data/Trending.js
@@ -2,7 +2,7 @@ const trendings= [
   {
     _id: "1",
     name: "ODODOS Unisex Mini Belt Bag with Adjustable Strap Small Waist Pouch for Workout Running Traveling Hiking, Black",
-    image: "/images/71.jpg",
+    image: "../images/71.jpg",
     link: "https://amzn.to/3qDHvY1",
     description:
       "➤Versatile Style: Adjustable Strap allow you to wear this belt bag in a variety of ways. It can be carried as cross bag, shoulder bag, waist bag or handbag. It’s all up to you.➤Functional: Our mini waist bag is lightweight and easy to carry, roomy for phone, wallet, passport, keys, ID and other small items, provide great convenience for you.➤High Quality: ODODOS mini belt bag are made of durable fabric, zippers and straps which is water resistant, durable and anti-abrasion, ensure a long-lasting usage.➤Design for: ODODOS mini belt bags ideal for men, women and teenagers, perfect for daily use, out door, gym workouts, running, biking, traveling, etc.➤Great Gift Option: Great idea as a special gift/present for her/his birthday, New Year, Valentine's Day, Easter, Mother's Day, Halloween, Thanksgiving and Christmas gifts .",
@@ -11,7 +11,7 @@ const trendings= [
   {
     _id: "2",
     name: "Brooks Women's Ghost 14 Neutral Running Shoe. Made in the USA or Imported. Rubber sole.",
-    image: "/images/72.jpg",
+    image: "../images/72.jpg",
     link: "https://amzn.to/45Wxg1q",
     description:" THIS WOMEN'S SHOE IS FOR: Runners looking for a smooth ride that won’t distract from the fun of the run. The Ghost 14 offers a simplified midsole construction which creates more seamless transitions. This Brooks Ghost 14 is a certified PDAC A5500 Diabetic shoe and has been granted the APMA Seal of Acceptance.SUPPORT AND CUSHION: The Ghost 14 offers neutral support while providing high energizing cushioning. Ideal for road running, cross training, the gym or wherever you might want to take them! Predecessor: Ghost 13 BALANCED, SOFT CUSHIONING: BioMoGo DNA and DNA LOFT cushioning work together to provide a just-right softness underfoot without losing responsiveness and durability - yet it feels lighter than ever. DNA LOFT now makes up the entire midsole, for a softer, smoother, ride. SMOOTH, STABLE RIDE: No matter how your foot lands, our Segmented Crash Pad - an integrated system of shock absorbers - will cushion every step and stride for smooth heel-to-toe transitions. SOFT, SECURE, FIT: More strategic use of the 3D Fit Print upper technology provides structure that moves with the foot."
     
@@ -19,7 +19,7 @@ const trendings= [
   {
     _id: "3",
     name: "SHAPERX Bodysuit for Women Tummy Control Shapewear Seamless Sculpting Thong Body Shaper Tank ",
-    image: "/images/73.jpg",
+    image: "../images/73.jpg",
     link: "https://amzn.to/448CGEX",
     description:
       "80% Nylon, 20% Spandex. Imported. Pull On closure. Hand Wash Only. Sizing Tips】Follow our size chart and measure your bust,waist and hip. You may be surprised how 'tiny' the bodysuit looks during 1st time try-on. But don't worry, the bodysuit is purposely designed with super stretchy fabric so that it will hug you in all right places! Tummy Control Bodysuit : This seamless shapewear holds in your core, lifts your chest, and smooths your abdomen. Its whisper-soft and seamless construction makes this sculpting piece a necessity for enhancing your body' natural shape. Comfortable Material: Thong full Bodysuit is made of high-quality fabric, lightweight, stretchy, and skin-friendly. The breast area is made of breathable mesh material, easy and comfortable for all-day long wearing.",
@@ -28,7 +28,7 @@ const trendings= [
   {
     _id: "4",
     name: "Hey Dude Wally Sox. Made in USA or Imported. Leather sole",
-    image: "/images/74.jpg",
+    image: "../images/74.jpg",
     link: "https://amzn.to/43QvRrf",
     description:
       "Rounded Toe Design and Flex & Fold: This supports healthy feet with its unique shape giving complete freedom of movement. The Hey Dude Wally Men’s Lace Up Loafers Comfortable & Light-Weight will be an instant favorite in your modern-casual shoe collection and are the ideal men’s shoes. Lightweight with Elastic Laces: Manufactured using an elastic material that allows them to flex with the foot, resulting in a shoe that fits more uniformly. Each shoe weighs 6 ounces or less making it one of the lightest pairs of shoes around. The ideal Hey Dude Shoes for Men that you will surely love. Comfortable Durable High-Quality Material: Designed with you in mind. This Wally shoe boasts the comfort of walking on clouds. Featuring an insole that reduces odors, lightweight outsole, and breathable cotton fabric that makes the Wally one of the most comfortable shoes on the market. Great For Travel & On The Go: The removable memory foam cushioned insole provides maximum comfort and reduces sweating and odors. Machine Washable Friendly (Cold). Sizing Tip: If you usually wear half sizes, we suggest choosing the next size up for the best overall fit.",
@@ -37,7 +37,7 @@ const trendings= [
   {
     _id: "5",
     name: "Crocs Unisex-Adult Classic Clog 100% Synthetic",
-    image: "/images/76.jpg",
+    image: "../images/76.jpg",
     link:"https://amzn.to/43vWGkS",
     description:
       "Imported. Ethylene Vinyl Acetate sole. Shaft measures approximately 8#inches from arch. Heel measures approximately 0.85",
@@ -46,7 +46,7 @@ const trendings= [
   {
     _id: "6",
     name: "VIFUUR Water Sports Shoes Barefoot Quick-Dry Aqua Yoga Socks Slip-on for Men Women Rubber sole",
-    image: "/images/77.jpg",
+    image: "../images/77.jpg",
     link:"https://amzn.to/3JrRTsP",
     description:
       "Recommended 1: CONVENIENCE - Smooth neck design prevents chafing when wearing our water shoes. It is convenient to wear and take off.Recommended 2: COMFORTABLE FIT -- Breathable and smooth fabrics with fine stretch on uppers. Like socks, flexible and comfortable. Recommended 3: RUBBER OUTSOLE & FOOT SAFETY -- Wearable and top-quality rubber sole, which protects your feet from being hurt by sharp objects. Recommended 4: OCCASION - Yoga Training, beach, swimming, pool, weight training, wake-boarding, sailing, boating, kayaking, windsurfing, cycling, jogging, walking, fishing, beach volleyball, gardening, lawn, car-washing and driving. Family outings! Tips：VARIOUS SIZE AVAILABLE -- fit different feet,little kids, big kids,men,women are available.",
@@ -55,7 +55,7 @@ const trendings= [
   {
     _id: "7",
     name: "BAGAIL 8 Set Packing Cubes Luggage Packing Organizers for Travel Accessories - Zipper closure",
-    image: "/images/78.jpg",
+    image: "../images/78.jpg",
     link: "https://amzn.to/3P3r2qw",
     description:
       "PREMIUM UPGRADED NYLON FABRIC. LIGHTWEIGHT&DURABLE. ORGANIZED PACKING and SPACE SAVING. VERSATILE and MULTI-FUNCTIONAL. MONEY BACK GUARANTEE: Offering you the best products and serving all your needs have always been our top goal. We stand behind all our BAGAIL products and offer an unconditional guarantee for the quality and craftsmanship in 12 months.",
@@ -64,7 +64,7 @@ const trendings= [
   {
     _id: "8",
     name: "Hey Dude Men's Wally Sox | Men's Loafers | Men's Slip On Shoes | Comfortable & Light-Weight",
-    image: "/images/79.jpg",
+    image: "../images/79.jpg",
     link: "https://www.amazon.com/Hey-Dude-Wally-Micro-Total/dp/B07H7SJ1KY?psc=1&linkCode=sl1&tag=almita375801-20&linkId=a3b045da9be874efdb4807b1957a5145&language=en_US&ref_=as_li_ss_tl",
     description:
       "Imported. Leather sole. Rounded Toe Design and Flex & Fold: This supports healthy feet with its unique shape giving complete freedom of movement. The Hey Dude Wally Men’s Lace Up Loafers Comfortable & Light-Weight will be an instant favorite in your modern-casual shoe collection and are the perfect men’s shoes. Lightweight with Elastic Laces: Manufactured using an elastic material that allows them to flex with the foot, resulting in a shoe that fits more uniformly. Each shoe weighs 6 ounces or less making it one of the lightest pairs of shoes around. The ideal Hey Dude Shoes for Men that you will surely love.Comfortable Durable High-Quality Material: Designed with you in mind. This Wally shoe boasts the comfort of walking on clouds. Featuring an insole that reduces odors, lightweight outsole, and breathable cotton fabric that makes the Wally one of the most comfortable shoes on the market. Great For Travel & On The Go: The removable memory foam cushioned insole provides maximum comfort and reduces sweating and odors. Machine Washable Friendly (Cold). Sizing Tip: If you usually wear half sizes, we suggest choosing the next size up for the best overall fit.",
@@ -73,7 +73,7 @@ const trendings= [
   {
     _id: "9",
     name: "Sweet Sweat Waist Trimmer for Women and Men - Sweat Band Waist Trainer for High-Intensity Training & Workouts, 5 Sizes",
-    image: "/images/80.jpg",
+    image: "../images/80.jpg",
     link: "https://amzn.to/3WUZR2Z",
     description:
       "Neoprene. Imported. Hook and Loop closure. Machine Wash. MAKE YOUR WORKOUT WORK FOR YOU: Elevate your next cardio, circuit, or HIIT session with the Sweet Sweat Waist Trimmer belt, designed to help you sweat and tone as you achieve your fitness goals. SWEAT FASTER, SWEAT HARDER: Waist trimmer designed with Neo-Sweat Technology, an anti-slip, moisture-repelling interior that provides superior heat insulation to increase core temperature for a harder, faster sweat and to cut excess water weight. CONTOURS TO YOUR BODY SHAPE: Designed to accommodate all shapes and sizes, our durable, machine-washable waist trainer has a velcro closure and adjusts to your body’s unique figure to provide a custom, contoured fit that looks as good as it feels",
