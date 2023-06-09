4ed15e9afa45a1c209e15d47eb05ec0df3356862
Alma Braun <aungonalna58@gmail.com>
2023-06-09 00:56:27 -0700

    Add files via upload

---
+++ src/components/TrendingProducts.js
@@ -0,0 +1,48 @@
+import React from "react";
+import trending from "../../data/Trending";
+
+function Trendingtrendings() {
+  return (
+    <>
+   
+    <div className="container mt-4">
+    <h2>Trending Best Sellers on Amazon</h2>
+      <div className="section">
+        <div className="row">
+          <div className="col-lg-12 col-md-12 article">
+            <div className="shopcontainer row">
+              <>
+                {trending.map((trending) => (
+                  <div
+                    className="shop col-lg-4 col-md-6 col-sm-6"
+                    key={trending._id}
+                  >
+                    <div className="border-trending">
+                      <a href={trending.link}>
+                        <div className="trendBack">
+                          <img src={trending.image} alt={trending.name} />
+                        </div>
+                      </a>
+
+                      <div className="shoptext">
+                        <p>
+                          <a href="https://amzn.to/3qDHvY1">
+                            {trending.name}
+                          </a>
+                        </p>
+                        <p className="trendDes">{trending.description}</p>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default Trendingtrendings;
